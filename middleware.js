cc9a0be13513cee083457460be8c505c55312650
Urooj-Zafar <uroojzafar116@gmail.com>
2026-08-29 14:54:24 +0500

    Update

--- middleware.js
+++ middleware.js
@@ -2,48 +2,33 @@ import { NextResponse } from "next/server";
 import { VerifyToken } from "@/helper/jwt";
 
 export function middleware(req) {
-  const token =
-    req.cookies.get("EliteShop")?.value;
-
+  const token = req.cookies.get("EliteShop")?.value;
   const { pathname } = req.nextUrl;
 
   if (pathname.startsWith("/admin")) {
-
     if (!token) {
-      return NextResponse.redirect(
-        new URL("/", req.url)
-      );
-    }
-
-    const payload = VerifyToken(token);
-
-    if (
-      !payload ||
-      payload.role?.toLowerCase() !== "admin"
-    ) {
-      return NextResponse.redirect(
-        new URL("/", req.url)
-      );
-    }
-  }
-
-
-  if (
-    pathname.startsWith("/BuyNow") ||
-    pathname.startsWith("/Checkout")
-  ) {
-
-    if (!token) {
-      return NextResponse.redirect(
-        new URL("/login", req.url)
+      return NextResponse.json(
+        { error: "NO TOKEN" },
+        { status: 401 }
       );
     }
 
     const payload = VerifyToken(token);
 
     if (!payload) {
-      return NextResponse.redirect(
-        new URL("/login", req.url)
+      return NextResponse.json(
+        { error: "INVALID TOKEN" },
+        { status: 401 }
+      );
+    }
+
+    if (payload.role?.toLowerCase() !== "admin") {
+      return NextResponse.json(
+        {
+          error: "NOT ADMIN",
+          role: payload.role,
+        },
+        { status: 403 }
       );
     }
   }
